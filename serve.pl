5db897d69697b4d05d2b0119b1c4928717ba9b2c
Max Maischein <corion@corion.net>
2020-01-25 15:21:22 +0100

    Serve the forecast online

--- serve.pl
+++ serve.pl
@@ -2,6 +2,14 @@
 use strict;
 use warnings;
 use Mojolicious::Lite;
+use Mojo::JSON qw(decode_json encode_json);
+
+use lib '../Weather-MOSMIX/lib';
+use Weather::MOSMIX;
+
+my $w = Weather::MOSMIX->new(
+    dsn => 'dbi:SQLite:dbname=../Weather-MOSMIX/db/forecast.sqlite',
+);
 
 # Route with placeholder
 #get '/:foo' => sub {
@@ -12,7 +20,27 @@ use Mojolicious::Lite;
 get '/' => sub {
     my $c = shift;
     $c->reply->static('index.html');
-  };
+};
+
+post '/forecast' => sub {
+    warn "Generating weather forecast";
+    my $c = shift;
+    my $query = decode_json( $c->req->body );
+    my @locations = @{ $query->{'locations'} };
+
+    my @res;
+    for my $l (@locations) {
+        my $f =
+            $w->forecast_dbh(latitude => $l->{latitude}, longitude => $l->{longitude} );
+        my $out = $w->format_forecast_dbh( $f, 6 );
+        push @res, $out;
+    };
+    use Data::Dumper; warn Dumper \@res;
+
+    $c->render(json => { forecast => \@res } );
+};
+
+
 
 # Start the Mojolicious command system
 app->start;
