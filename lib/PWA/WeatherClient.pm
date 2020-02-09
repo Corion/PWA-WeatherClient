83866242ec05a51876b7d0b6029d6e6680daf9ec
Max Maischein <corion@corion.net>
2020-02-09 16:12:21 +0100

    Add a list of items still to implement

--- lib/PWA/WeatherClient.pm
+++ lib/PWA/WeatherClient.pm
@@ -4,7 +4,35 @@ our $VERSION = '0.01';
 
 =head1 NAME
 
-PWA::WeatherClient - placeholder
+PWA::WeatherClient - placeholder for the PWA of the client
+
+=head1 TO DO
+
+=over 4
+
+=item *
+
+Periodically refresh weather data if it is outdated
+
+=item *
+
+Implement location selection via a C<< <datalist> >> list of the (5000)
+locations available in the MOSMIX set. This means no (client side)
+search/ordering by location/vincinity, but it also means very little code
+outside of what is needed to create/update the C<< <datalist> >> items.
+
+Maybe these locations can be (one-time) cached client-side as JSON and
+used to construct the template for location selection instead?!
+
+=item *
+
+Use the background sync API instead of (always) doing a HTTP request
+
+This would make fetching the updated data more resilient and power saving
+in the cases where the browser supports the Background Sync API. Currently
+this means using Chrome, as Firefox does not support the API.
+
+=back
 
 =cut
 
